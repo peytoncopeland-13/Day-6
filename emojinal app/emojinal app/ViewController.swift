7e0b5dae418f3f733b863d60a74cbaad4499abb4
Peyton <peyton@apples-macbook-pro-50.local>
2019-08-12 11:58:40 -0700

    Iteration 2

--- emojinal app/emojinal app/ViewController.swift
+++ emojinal app/emojinal app/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = ["😂":"LOL", "🤩":"star eyes"]
+    
+    let customMessage = ("LOL", "How Funny", " Good joke", "Superstar", "Your a star", "Go you")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showMessage(sender: UIButton){
+        
+       
+        if let selectedEmotion = sender.titleLabel?.text{
+       
+            let titleText = selectedEmotion
+            let messageText = "😂"
+            
+            let emojiMessage = customMessage[emojis[selectedEmotion]!]?[0]
+            
+        let alertController = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert
+            
+                    let alertController = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    
+                    present(alertController, animated: true, completion: nil)
+        }
+        }
 
 }
-
